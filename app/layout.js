c32fd3f6653758b2b2b0b5b3f1b86c99d8bc5b8d
Santosh <rajvi.ecom@gmail.com>
2026-07-07 21:04:33 +0530

    Add new independent GA4 tracking

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 ﻿import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import SessionProvider from "@/components/SessionProvider";
@@ -25,6 +26,15 @@ export default async function RootLayout({ children }) {
   const session = await getServerSession(authOptions);
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-CV49J65FXZ" strategy="afterInteractive" />
+      <Script id="ga4" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CV49J65FXZ');
+        `}
+      </Script>
       <head>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning={true}>
